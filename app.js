c7b30f196b97be2ae2dbbb2c87b0380dfd5d0b61
Andy Nguyen <andyn.network@gmail.com>
2023-08-10 13:53:21 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const choices = ["rock", "paper", "scissors"];
 const resultEl = document.getElementById("result");
 const messageEl = document.getElementById("message");
 const newGameEl = document.getElementById("newGame");
+const winCountEl = document.getElementById("win");
+const loseCountEl = document.getElementById("lose");
+const drawCountEl = document.getElementById("draw");
+
+let winCount = 0;
+let loseCount = 0;
+let drawCount = 0;
 
 function computerPlay() {
     return choices[Math.floor(Math.random() * choices.length)];
@@ -11,6 +18,8 @@ function playRound(playerChoice, computerChoice) {
     if (playerChoice === computerChoice) {
         resultEl.textContent = "Draw";
         messageEl.textContent = "It's a TIE!";
+        drawCount++;
+        drawCountEl.textContent = "Draws: " + drawCount;
     } 
     else if (
         (playerChoice === "rock" && computerChoice === "scissors") || 
@@ -19,12 +28,16 @@ function playRound(playerChoice, computerChoice) {
      ) {
         resultEl.textContent = "You Win!";
         messageEl.textContent = "Player's " + (playerChoice).toUpperCase() + " beats " + (computerChoice).toUpperCase();
+        winCount++;
+        winCountEl.textContent = "Wins: " + winCount;
     } 
     else {
         console.log("Player: " + playerChoice);
         console.log("Opponent: " + computerChoice);
         resultEl.textContent = "You Lose!";
         messageEl.textContent = "Computer's " + (computerChoice).toUpperCase() + " beats " + (playerChoice).toUpperCase();
+        loseCount++;
+        loseCountEl.textContent = "Losses: " + loseCount;
     }
 }
 
